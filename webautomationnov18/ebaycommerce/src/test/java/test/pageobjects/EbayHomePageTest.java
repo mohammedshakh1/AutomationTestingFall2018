fac290de06eb5bceddc51e5031c77f91536d07d3
Asif Shakh <asif@Asifs-MacBook-Pro.local>
2018-12-26 01:43:32 -0500

    first commit

---
+++ webautomationnov18/ebaycommerce/src/test/java/test/pageobjects/EbayHomePageTest.java
@@ -0,0 +1,48 @@
+package test.pageobjects;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import page.objects.EbayHomePage;
+
+public class EbayHomePageTest extends EbayHomePage {
+
+    EbayHomePage objOfEbayHomePage = null;
+
+    @BeforeMethod
+    public void initializationOfElements(){
+        objOfEbayHomePage = PageFactory.initElements(driver, EbayHomePage.class);
+
+    }
+
+    @Test
+    public void searchBoxTest(){
+        objOfEbayHomePage.searchBox();
+        System.out.println("Search Box Test Passed");
+    }
+
+    @Test
+    public void eBayLogoDisplayTest(){
+        boolean eBayLogo = objOfEbayHomePage.eBayLogoDisplay();
+        Assert.assertEquals(eBayLogo, true);
+        System.out.println("Ebay Logo Test Passed");
+    }
+
+    @Test
+    public void untilEndTheSeasonDeals(){
+        String
+    }
+
+    @Test
+    public void dailyDeals(){
+        String dealsText = objOfEbayHomePage.clickDailyDeals();
+    }
+
+
+
+
+
+
+
+}
